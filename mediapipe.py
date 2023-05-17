17e73c1a0197cc2a6280aaf4e471e80fe1189425
felipe.narvaez <felipe.98np@outlook.com>
2023-05-17 16:38:44 -0500

    cambios en el codigo

--- mediapipe.py
+++ mediapipe.py
@@ -3,17 +3,30 @@ import numpy as np
 import imutils
 import os
 
-captura = cv2.VideoCapture(cv2.CAP_ANY)
+umbral=100
+valor_maximo=255
 
-while(captura.isOpened()):
+captura = cv2.VideoCapture(0) # para que detecte la camara es 0
+
+
+while True:
     ret,imagen = captura.read() # devuelve 2 argumentos, 1 es bool y el otro es la imagen
-    if ret == True:
-        cv2.imshow('video',imagen)
-        cv2.waitkey(0)
+    _, th = cv2.threshold(imagen, umbral, valor_maximo, cv2.THRESH_BINARY_INV)
+
+    cv2.imshow('video',imagen)
+    cv2.imshow('video',th)
+        # saca 1 frame x seg, si es 5000 seria 1 frame cada 5 segundos,
+        # si es 0 la ventana se despliega infinita como imagen hasta que presiones una tecla
+
+        #0xFF es para que detecte el valor en una maquina x64
+        # ord recibe un caracter y regresa el unicode
+
+
+    if cv2.waitKey(1) & 0xFF == ord('x'):
+        break
 
 
 captura.release()
-#cv2.waitkey(0)
 cv2.destroyAllWindows()
 
 
