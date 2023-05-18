ccf8bab842b6b7258ef5580a6f8bb34dc6baac01
felipe.narvaez <felipe.98np@outlook.com>
2023-05-18 16:38:53 -0500

    version 3

---
+++ dibujar.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+
+celeste1 = np.array([75,185,88],np.uint8)
+celeste2 = np.array([112,255,255],np.uint8)
+
+colorCeleste = (255,113,82)
+colorRosa = (128,0,255)
+colorVerde = (0,255,36)
+colorLimpiar = (29,112,246)
+
+
