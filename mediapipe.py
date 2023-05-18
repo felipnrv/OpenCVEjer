c0ee4f1fe713fc1266eb82e19c9e0f58de5927b9
felipe.narvaez <felipe.98np@outlook.com>
2023-05-18 11:51:50 -0500

    version 2.0

--- mediapipe.py
+++ mediapipe.py
@@ -5,7 +5,6 @@ valor_maximo=255
 
 captura = cv2.VideoCapture(0) # para que detecte la camara es 0
 
-
 while True:
     # devuelve 2 argumentos, 1 es bool y el otro es la imagen
     ret, imagen = captura.read()
@@ -13,13 +12,13 @@ while True:
     gris=cv2.cvtColor(th, cv2.COLOR_BGR2GRAY)
     contornos, _ = cv2.findContours(gris, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     #print('Contornos: ',len(contornos))
-    #texto = "Contornos: "+ str(len(contornos))
+    texto = "Contornos: "+ str(len(contornos))
 
     # el -1 es para que se dibujen todos los contornos
     # el 2 es el grosor de la linea y el color es el 255,0,0
     cv2.drawContours(th,contornos,-1,(255,0,0),20)
     #pone el texto de los contornos
-    #cv2.putText(imagen,texto ,(10,20),cv2.FONT_HERSHEY_SIMPLEX,0.7,(255,0,0),1)
+    cv2.putText(imagen,texto ,(10,20),cv2.FONT_HERSHEY_SIMPLEX,0.7,(255,0,0),1)
 
     cv2.imshow('video',imagen) # es lo que ve el usuario
     #cv2.imshow('video',th) 
@@ -32,7 +31,6 @@ while True:
 
     if cv2.waitKey(1) & 0xFF == ord('x'):
         break
-
 
 captura.release()
 cv2.destroyAllWindows()
